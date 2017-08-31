718fb2ba6e669e079cb0628e41687390ab6510ed
Juan C Garcia <Juan.Garcia@generalassemb.ly>
2017-08-31 06:04:30 -0400

    standard-js compliance

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const express = require('express');
-var app = express();
+const express = require('express')
+var app = express()
 
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 app.set('port', process.env.PORT || 3001)
 
-app.get('/', (req, res)=>{
-    // res.send('hello!')
-    res.sendFile(__dirname + '/index.html')
+app.get('/', (req, res) => {
+  // res.send('hello!')
+  res.sendFile(__dirname + '/index.html')
 })
 
-io.on('connection', socket=>{
-  console.log('a user connected');
-  socket.on('disconnect', ()=>{
-      console.log('user disconnected')
+io.on('connection', socket => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
   })
-  socket.on('chat message', (message)=>{
+  socket.on('chat message', (message) => {
     io.emit('chat message', message)
     // socket.broadcast.emit('chat message', message)
     console.log(`chat message: ${message}`)
   })
-});
+})
 
-http.listen(app.get('port'), (err)=>{
-    console.log(`connected on port ${app.get('port')}`)
+http.listen(app.get('port'), (err) => {
+  if (err) {
+    console.log('Error', err)
+  }
+  console.log(`connected on port ${app.get('port')}`)
 })
